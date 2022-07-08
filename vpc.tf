43c34318f2375f7f2c446a373bb90cd28409a8b0
ashish mittal <amittal071@gmail.com>
2022-07-08 09:08:15 +0000

    bastion and private host

--- vpc.tf
+++ vpc.tf
@@ -38,24 +38,24 @@ resource "aws_internet_gateway" "terraform_igw" {
   }
 }
 
-resource "aws_route_table" "terraform_route_table" {
+resource "aws_route_table" "terraform_public_route_table" {
   vpc_id = aws_vpc.terraform_vpc.id
 
   tags = {
-    Name = "terraform-route-table"
+    Name = "terraform-public-route-table"
   }
 }
 
-resource "aws_route_table_association" "terraform_association" {
+resource "aws_route_table_association" "terraform_public_route_table_association" {
   subnet_id      = aws_subnet.terraform_public_subnet.id
-  route_table_id = aws_route_table.terraform_route_table.id
+  route_table_id = aws_route_table.terraform_public_route_table.id
 }
 
-resource "aws_route" "terraform_route" {
-  route_table_id         = aws_route_table.terraform_route_table.id
+resource "aws_route" "terraform_public_route" {
+  route_table_id         = aws_route_table.terraform_public_route_table.id
   gateway_id             = aws_internet_gateway.terraform_igw.id
   destination_cidr_block = "0.0.0.0/0"
-  depends_on             = [aws_route_table.terraform_route_table]
+  depends_on             = [aws_route_table.terraform_public_route_table]
 }
 
 
@@ -96,6 +96,14 @@ resource "aws_security_group" "terraform_allow_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
@@ -110,7 +118,7 @@ resource "aws_security_group" "terraform_allow_ssh" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "terraform_public_instance" {
   depends_on                  = [aws_internet_gateway.terraform_igw]
   associate_public_ip_address = true
   ami                         = data.aws_ami.amazon-linux-2.id
@@ -118,8 +126,70 @@ resource "aws_instance" "web" {
   key_name                    = "officeyahoo"
   subnet_id                   = aws_subnet.terraform_public_subnet.id
   vpc_security_group_ids      = ["${aws_security_group.terraform_allow_ssh.id}"]
-
+  user_data                   = file("app1-install.sh")
   tags = {
-    Name = "HelloWorld"
+    Name = "Terraform-Public-Instance"
   }
 }
+
+resource "aws_subnet" "terraform_private_subnet" {
+  vpc_id            = aws_vpc.terraform_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "terraform-private-subnet"
+  }
+}
+
+resource "aws_eip" "terraform_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.terraform_igw]
+}
+
+resource "aws_nat_gateway" "terraform_nat_gateway" {
+  allocation_id = aws_eip.terraform_eip.id
+  subnet_id     = aws_subnet.terraform_private_subnet.id
+
+  tags = {
+    Name = "terraform-nat-gateway"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.terraform_igw]
+}
+
+
+resource "aws_route_table" "terraform_private_route_table" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  tags = {
+    Name = "terraform-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "terraform_private_route_table_association" {
+  subnet_id      = aws_subnet.terraform_private_subnet.id
+  route_table_id = aws_route_table.terraform_private_route_table.id
+}
+
+resource "aws_route" "terraform_private_route" {
+  depends_on             = [aws_nat_gateway.terraform_nat_gateway]
+  route_table_id         = aws_route_table.terraform_private_route_table.id
+  gateway_id             = aws_nat_gateway.terraform_nat_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_instance" "terraform_private_instance" {
+  depends_on             = [aws_nat_gateway.terraform_nat_gateway]
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t2.micro"
+  key_name               = "officeyahoo"
+  subnet_id              = aws_subnet.terraform_private_subnet.id
+  vpc_security_group_ids = ["${aws_security_group.terraform_allow_ssh.id}"]
+  tags = {
+    Name = "Terraform-Private-Instance"
+  }
+}
+
