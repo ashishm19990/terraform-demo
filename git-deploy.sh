8f479b3c827b335a99445705d86aa26b21117323
ashish mittal <ashish>
2022-07-18 22:02:25 +0530

    Welcome To Vardhman Garments

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Add files and do local commit"
 git add .
-git commit -am "Welcome In Vardhman Garments"
+git commit -am "Welcome To Vardhman Garments"
 
 echo "Pushing to Github Repository"
 git push
